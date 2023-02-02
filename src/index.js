94f636a7b84c65b338e63e2f0704eadf00437879
Ouail-01 <ouaillaarabi@gmail.com>
2023-02-02 07:40:46 +0100

    Fix the linters

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './style.css';
-import { leaderboard } from './modules/api';
-import postScore from './modules/score';
+import { leaderboard } from './modules/api.js';
+import postScore from './modules/score.js';
 
 const playerName = document.querySelector('#player-name');
 const playerScore = document.querySelector('#player-score');
@@ -9,20 +9,20 @@ const refresh = document.querySelector('#refresh');
 const submit = document.querySelector('#submit');
 
 submit.addEventListener('click', (e) => {
-    e.preventDefault();
-    if(playerName.value === '' || playerScore.value === '') {
-        playerName.reportValidity();
-        playerScore.reportValidity();
-    }else {
-        leaderboard(playerName.value, playerScore.value);
-        postScore(score);
-        playerName.value = '';
-        playerScore.value = '';
-    }
-})
+  e.preventDefault();
+  if (playerName.value === '' || playerScore.value === '') {
+    playerName.reportValidity();
+    playerScore.reportValidity();
+  } else {
+    leaderboard(playerName.value, playerScore.value);
+    postScore(score);
+    playerName.value = '';
+    playerScore.value = '';
+  }
+});
 
 refresh.addEventListener('click', () => {
-    window.location.reload();
+  window.location.reload();
 });
 
 postScore(score);
--- src/modules/api.js
+++ src/modules/api.js
@@ -1,24 +1,24 @@
 const leaderboard = async (userName, userScore) => {
-    const results = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/XpNfvUYHUwTMbBN5edTb/scores/', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json; charset=utf-8',
-        },
-        body: JSON.stringify ({
-            user : userName,
-            score : userScore,
-        }),
-    })
+  const results = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/XpNfvUYHUwTMbBN5edTb/scores/', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json; charset=utf-8',
+    },
+    body: JSON.stringify({
+      user: userName,
+      score: userScore,
+    }),
+  });
 
-    const jsonData = await results.json();
-    return jsonData;
-}
+  const jsonData = await results.json();
+  return jsonData;
+};
 const dynUsers = (i, cnt) => {
-    let players = '';
-    i.forEach((player) => {
-        players += `<h4>${player.user} : ${player.score}</h4>`
-    });
-    cnt.innerHTML = players;
-}
+  let players = '';
+  i.forEach((player) => {
+    players += `<h4>${player.user} : ${player.score}</h4>`;
+  });
+  cnt.innerHTML = players;
+};
 
-export {leaderboard, dynUsers};
+export { leaderboard, dynUsers };
