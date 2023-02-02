65341331d488a0a2f5e056f239e17c8ebbf9e364
Ouail-01 <ouaillaarabi@gmail.com>
2023-02-02 07:22:52 +0100

    Add the submit click listener

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
 import './style.css';
+import { leaderboard } from './modules/api';
+import postScore from './modules/score';
+
+const playerName = document.querySelector('#player-name');
+const playerScore = document.querySelector('#player-score');
+const score = document.querySelector('#score');
+const submit = document.querySelector('#submit');
+
+submit.addEventListener('click', (e) => {
+    e.preventDefault();
+    if(playerName.value === '' || playerScore.value === '') {
+        playerName.reportValidity();
+        playerScore.reportValidity();
+    }else {
+        leaderboard(playerName.value, playerScore.value);
+        postScore(score);
+        playerName.value = '';
+        playerScore.value = '';
+    }
+})
